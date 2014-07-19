125b32233248b4302b71093f3ed10b3c61967824
Mike <pma88@list.ru>
2014-07-19 22:51:03 +0400

    divide test

--- unittest_profile2.py
+++ unittest_profile2.py
@@ -81,7 +81,7 @@ class Test_profile(unittest.TestCase):
         self.assertLess(abs(el.jx()-1592.8845)/abs(el.jx()),0.001)
         self.assertLess(abs(el.jy()-1592.8845)/abs(el.jx()),0.001)
         self.assertLess(abs(el.jxy()+1305.7566)/abs(el.jxy()),0.001)
-        print 8       
+        print 8       ,       "test_quartercirclesolid" 
     def test_anglerectangle(self):
         el=anglerectangle(b=15, h=10)
         self.assertLess(abs(el.a()-15*10/2)/abs(el.a()), 0.00001)
@@ -108,7 +108,7 @@ class Test_profile(unittest.TestCase):
         self.assertLess(abs(el.jx()-pi*30**4/64*(1-(10./15)**4))/abs(el.jx()),0.001)
         self.assertLess(abs(el.jy()-pi*30**4/64*(1-(10./15)**4))/abs(el.jx()),0.001)
         self.assertEqual(el.jxy(),0) 
-        print 11       
+        print 11        ,       "test_ring"      
     def test_angle(self):
         el=angle(b=10, h=12, x1=3)
         self.assertLess(abs(el.a()-1/2.*10*12)/abs(el.a()), 0.00001)
@@ -116,7 +116,7 @@ class Test_profile(unittest.TestCase):
         self.assertLess(abs(el.yi()-4)/abs(el.yi()),0.00001)
         self.assertLess(abs(el.jx()-480)/abs(el.jx()),0.001)
         self.assertLess(abs(el.jy()-263.333)/abs(el.jx()),0.001)
-        print 12       
+        print 12         ,       "test_angle"
     def test_circleseg(self):
         el=circleseg(a1=pi/6, r=10)
         self.assertLess(abs(el.a()- 9.0586)/abs(el.a()), 0.00001)
@@ -125,7 +125,7 @@ class Test_profile(unittest.TestCase):
         self.assertLess(abs(el.jx()-1.1183)/abs(el.jx()),0.001)
         self.assertLess(abs(el.jy()-46.0432)/abs(el.jx()),0.001)
         self.assertEqual(el.jxy(),0)  
-        print 13       
+        print 13         ,       "test_circleseg"
     def test_krug(self):
         el=krug(a1=pi/6, r=10)
         self.assertLess(abs(el.a()- 5.3751)/abs(el.a()), 0.00001)
@@ -135,7 +135,7 @@ class Test_profile(unittest.TestCase):
         self.assertLess(abs(el.jy()-14.0974)/abs(el.jx()),0.001)
         self.assertEqual(el.jxy(),0)
         
-        print 14       
+        print 14       ,       " test_krug"
     def test_rotkrug(self):
         el=rotkrug(a1=pi/6, r=10)
         self.assertLess(abs(el.a()- 5.3751)/abs(el.a()), 0.00001)
@@ -144,7 +144,7 @@ class Test_profile(unittest.TestCase):
         self.assertLess(abs(el.jy()- 10.8752)/abs(el.jy()),0.001)
         self.assertLess(abs(el.jx()-4.4307)/abs(el.jx()),0.001)
         self.assertLess(abs(el.jxy()+5.5811)/abs(el.jxy()),0.001)
-        print 15       
+        print 15        ,       " test_rotkrug"
     def test_dvut(self):
         el=dvut(h=60, b=19, t=1.78, s=1.2, r1=2, r2=0.8, a1=atan(12./100))
         self.assertLess(abs(el.a()- 137.5384332)/abs(el.a()), 0.0000001)
@@ -271,7 +271,7 @@ class Test_profile(unittest.TestCase):
         self.assertLess(abs(el.af()-33.82)/abs(el.af()),0.000002)
 
         self.assertLess(abs(el.afaw()-33.82/67.73)/abs(el.afaw()),0.0002) 
-        print 24                  
+        print 24   ,       " test_dvut"                
     def test_dvut2(self):
         el=dvut(h=360., b=130., t=16., s=9.5, r1=14., r2=6., a1=atan(12./100))
         self.assertLess(abs(el.jx()- 15339.998*10**4)/abs(el.jx()),0.001)
@@ -303,15 +303,273 @@ class Test_profile(unittest.TestCase):
 #        print el3.jt()
         self.assertLess(abs(el3.jt()-31.22*10**4)/abs(el3.jt()),0.005)   
 
-        print 16                 
+        print 16  ,       " test_dvut2"                    
     def test_function(self):
         self.assertEqual(jxdx(5,6,7),299)      
         self.assertLess((jda(11.25, 31.25, 0, 45./180*pi)[0]-21.25)/21.25,0.00002)          
         self.assertLess((jda(11.25, 31.25, 0, 45./180*pi)[1]-21.25)/21.25,0.00002)
         self.assertLess((jda(11.25, 31.25, 0, 45./180*pi)[2]-10.)/10.,0.00002)
-        print 17               
+        print 17      ,       "test_function"         
 #         self.assertLess(abs(el.get_sigma_e()-3.5412957)/abs(el.get_sigma_e()),0.0000001)        
 
+    def test_korob(self):
+        pr1=truba_pryam(h=12,b=8,t=0.6, s=0.6, r2=1.2, r1=0.6)
+#        print pr1.a()
+        self.assertLess(abs(pr1.a()-21.63)/21.63,0.001) 
+#        print pr1.jx()
+        self.assertLess(abs(pr1.jx()-405.9)/405.9,0.001)  
+#        print pr1.jy()
+        self.assertLess(abs(pr1.jy()-215)/215,0.001)  
+#        print pr1.s2x()       
+        self.assertLess(abs(pr1.s2x()-42.1)/42.1,0.001) 
+#        print pr1.s2y()    
+        self.assertLess(abs(pr1.s2y()-31.77)/31.77,0.001) 
+#        print pr1.s2x()       
+        self.assertLess(abs(pr1.sx1()-1.44)/1.44,0.001) 
+#        print pr1.s2y()    
+        self.assertLess(abs(pr1.sy1()-2.16)/2.16,0.001) 
+        
+        self.assertLess(abs(pr1.aw()-06.48)/06.48,0.001) 
+
+        self.assertLess(abs(pr1.af()-4.8)/4.8,0.001)
+
+        self.assertLess(abs(pr1.afaw()-0.741/2)/0.741*2,0.001)        
+        
+        self.assertLess(abs(pr1.hef()-9.6)/9.6,0.001) 
+
+        self.assertLess(abs(pr1.bef()-5.6)/5.6,0.001) 
+
+        self.assertEqual(pr1.title(),'korob')     
+        print 18      ,       "test_korob"   
+    def test_ugol(self):
+        print 30     ,       "test_ugol" 
+
+
+        pr1=ugol(h=18,b=18,t=1.5, s=1.5, r2=0, r1=1.6, r3=0.53)
+
+#        print pr1.ix0()
+#        print pr1.iy0()
+#        
+        self.assertLess(abs(pr1.a()-52.18)/52.18,0.0001)
+        self.assertLess(abs(pr1.jx()-1607.36)/1607.36,0.0001)
+        self.assertLess(abs(pr1.jy()-1607.36)/1607.36,0.0001)
+        self.assertLess(abs(pr1.jxy()+947.6)/947.6,0.0001)
+        self.assertLess(abs(pr1.jx0()-2555)/2555,0.0001)
+        self.assertLess(abs(pr1.jy0()-659.73)/659.73,0.0001)
+        self.assertLess(abs(pr1.alpha()-atan(1))/1,0.0001)
+        self.assertLess(abs(pr1.ix0()-6.997578)/6.997578,0.0001)
+        self.assertLess(abs(pr1.iy0()-3.5558)/3.5558,0.0001)
+
+        self.assertLess(abs(pr1.hef()-(18-1.5-1.6))/1,0.0001)        
+        self.assertLess(abs(pr1.bef()-(18-1.5-1.6))/1,0.0001) 
+
+
+        self.assertLess(abs(pr1.dx()-5.01)/5.01,0.0001)
+        self.assertLess(abs(pr1.dy()-5.01)/5.01,0.0001)
+
+
+        pr1=ugol(h=11,b=7,t=0.65, s=0.65, r2=0, r1=1.0, r3=0.33)
+
+#        print pr1.ix0()
+#        print pr1.dy()
+#        
+        self.assertLess(abs(pr1.a()-11.4454)/11.4454,0.0001)
+        self.assertLess(abs(pr1.jx()-142.42)/142.42,0.0001)
+        self.assertLess(abs(pr1.jy()-45.61)/45.61,0.0001)
+        self.assertLess(abs(pr1.jxy()+46.435)/46.435,0.0001)
+#        self.assertLess(abs(pr1.jx0()-2555)/2555,0.0001)
+        self.assertLess(abs(pr1.jy0()-26.937)/26.937,0.0001)
+        self.assertLess(abs(pr1.tanalpha()-0.40208)/0.40208,0.0001)
+#        self.assertLess(abs(pr1.ix0()-6.997578)/6.997578,0.0001)
+        self.assertLess(abs(pr1.iy0()-1.53413)/1.53413,0.0001)
+
+        self.assertLess(abs(pr1.hef()-(11-0.65-1.0))/1,0.0001)        
+        self.assertLess(abs(pr1.bef()-(7-0.65-1.0))/1,0.0001) 
+
+
+        self.assertLess(abs(pr1.dx()-1.58348)/1.58348,0.0001)
+        self.assertLess(abs(pr1.dy()-3.545982)/3.55,0.0001)
+
+
+        pr1=ugol(h=12,b=12,t=0.6, s=0.6, r2=0.9+0.6, r1=0.9, r3=0.0)
+
+#        print pr1.dx()
+#        print pr1.jxy()
+#        
+        self.assertLess(abs(pr1.a()-13.73)/13.73,0.0001)
+        self.assertLess(abs(pr1.jx()-197.46)/197.46,0.0001)
+        self.assertLess(abs(pr1.jy()-197.46)/197.46,0.0001)
+#        self.assertLess(abs(pr1.jxy()+346.44)/346.44,0.0001)
+        self.assertLess(abs(pr1.jx0()-320.48)/320.48,0.0001)
+        self.assertLess(abs(pr1.jy0()-74.44)/74.44,0.0001)
+        self.assertLess(abs(pr1.tanalpha()-1)/1,0.0001)
+        self.assertLess(abs(pr1.ix0()-4.83114)/4.83114,0.0001)
+        self.assertLess(abs(pr1.iy0()-2.32837)/2.32837,0.0001)
+
+        self.assertLess(abs(pr1.hef()-(12-0.6-0.9))/1,0.0001)        
+        self.assertLess(abs(pr1.bef()-(12-0.6-0.9))/1,0.0001) 
+
+
+        self.assertLess(abs(pr1.dx()-3.293973)/3.29,0.0001)
+        self.assertLess(abs(pr1.dy()-3.293973)/3.29,0.0001)
+        
+    def test_shvel(self):
+        print 31    , "test_shvel" 
+
+        pr1=shvel(h=27,b=9.5,t=1.05, s=0.6, r2=0.45, r1=1.1, r3=0.0, a1=atan(0.1))
+
+#        print pr1.jy()
+#        print pr1.jx()
+#        print pr1.w2()
+        
+        self.assertLess(abs(pr1.a()-35.2313888)/35.2313888,0.0001)
+        self.assertLess(abs(pr1.jx()-4163.337068)/4163.337068,0.0001)
+        self.assertLess(abs(pr1.jy()-261.7465238)/261.7465238,0.0001)        
+
+        self.assertLess(abs(pr1.dx()-2.474374)/2.47,0.0001) 
+        
+        self.assertLess(abs(pr1.s2x()-177.608)/177.608,0.0001)
+        self.assertLess(abs(pr1.s2y()-40.9452)/40.9452,0.0001) 
+
+        self.assertLess(abs(pr1.t1()-0.605)/.605,0.0001)
+        self.assertLess(abs(pr1.t2()-0.89)/0.89,0.0001)
+
+
+        self.assertLess(abs(pr1.hef()-22.02)/22.02,0.0001)
+        self.assertLess(abs(pr1.bef()-7.91)/7.91,0.0001)
+     
+
+        pr1=shvel(h=20,b=10,t=0.6, s=0.6, r2=0, r1=0.9, r3=0.9+0.6, a1=atan(0.0))
+
+#        print pr1.jy()
+#        print pr1.jx()
+#        print pr1.dx()
+        
+        self.assertLess(abs(pr1.a()-22.66)/22.66,0.0001)
+        self.assertLess(abs(pr1.jx()-1400.82)/1400.82,0.0001)
+        self.assertLess(abs(pr1.jy()-224.37)/224.37,0.0001)        
+
+        self.assertLess(abs(pr1.dx()-2.794946)/2.79,0.0001) 
+        
+        self.assertLess(abs(pr1.s2x()-81.64)/81.64,0.0001)
+        self.assertLess(abs(pr1.s2y()-31.148)/31.148,0.0001) 
+
+        self.assertLess(abs(pr1.t1()-0.6)/.001,0.0001)
+        self.assertLess(abs(pr1.t2()-0.0)/0.001,0.0001)
+
+
+        self.assertLess(abs(pr1.hef()-17)/17,0.0001)
+        self.assertLess(abs(pr1.bef()-8.5)/8.5,0.0001)
+        
+        pr1=shvel(h=30,b=8.5,t=1.35, s=0.75, r2=0.0, r1=0.0, r3=0.0, a1=atan(0.1))
+
+#        print pr1.jt()
+
+        self.assertLess(abs(pr1.w1()-37.21)/37.21,0.03)
+        self.assertLess(abs(pr1.w2()-76.54)/76.54,0.03)
+        
+        self.assertLess(abs(pr1.xa()-2.26)/2.26,0.03)
+
+        self.assertLess(abs(pr1.jw()-36645)/36645,0.01)
+        
+        self.assertLess(abs(pr1.jt()-20.39)/20.39,0.03)
+        
+        pr1=shvel(h=30,b=10,t=1.1, s=0.65, r2=0.5, r1=1.2, r3=0.0, a1=atan(0.1))
+
+#        print pr1.jt()
+        self.assertLess(abs(pr1.w1()-43.3)/43.3,0.04)
+        self.assertLess(abs(pr1.w2()-92.8)/92.8,0.03)         
+        self.assertLess(abs(pr1.xa()-2.72)/2.72,0.041) 
+        
+        self.assertLess(abs(pr1.jw()-45640)/45640,0.03)        
+
+        self.assertLess(abs(pr1.jt()-12.8)/12.8,0.03) 
+
+        self.assertLess(abs(pr1.jt_sp()-12.8/1.12)/12.8,0.03) 
+        
+        pr1=shvel(h=30,b=10,t=1.1, s=0.65, r2=0.7, r1=1.2, r3=0.0, a1=atan(0.0))
+
+#        print pr1.jw()
+        self.assertLess(abs(pr1.w1()-46.7)/46.7,0.05)
+        self.assertLess(abs(pr1.w2()-93.1)/93.1,0.03)         
+        self.assertLess(abs(pr1.xa()-2.9)/2.9,0.05) 
+        
+        self.assertLess(abs(pr1.jw()-59795)/59795,0.03)        
+
+        self.assertLess(abs(pr1.jt()-12.8)/12.8,0.03) 
+
+        self.assertLess(abs(pr1.jt_sp()-12.8/1.12)/12.8,0.03) 
+    def test_sostav(self):
+        print 32  , "test_sostav"   
+
+        pr1=ugol(h=18,b=18,t=1.5, s=1.5, r2=0, r1=1.6, r3=0.53)
+        pr2=ugol(h=18,b=18,t=1.5, s=1.5, r2=0, r1=1.6, r3=0.53)
+        
+        pr=profiles_sostav(pr1=pr1, alpha1=0, x1=0.3, y1=0, mir1=0, pr2=pr2, alpha2=0, x2=-0.3,y2=0,mir2=1)
+        pr.solve()
+#        print pr1.jy()
+#        print pr1.jx()
+#        print pr.alpha()
+        
+        self.assertLess(abs(pr.a()-52.18*2)/52.18/2,0.0001)        
+        self.assertLess(abs(pr.jx()-1607.36*2)/1607.36/2,0.0001) 
+        self.assertLess(abs(pr.jy()-6157.265)/6157.265,0.0001) 
+
+
+
+
+        
+        self.assertLess(abs(pr.xi()-0)/0.001,0.0001)
+        self.assertLess(abs(pr.yi()-5.01)/5.01,0.0001)
+
+        self.assertLess(abs(pr.xi()-0)/0.001,0.0001)
+        self.assertLess(abs(pr.yi()-5.01)/5.01,0.0001)
+
+        pr1=ugol(h=16,b=10,t=1.0, s=1.0, r2=0.0, r1=1.3, r3=0.43)
+
+
+        pr=profiles_sostav(pr1=pr1, alpha1=pi, x1=0.3, y1=0.3, mir1=0, pr2=pr1, alpha2=0, x2=-0.3-2.28*2,y2=-0.3-5.23*2,mir2=0)
+        pr.solve()
+#        print pr.jxy()
+        
+        self.assertLess(abs(pr.a()-25.283*2)/25.283/2,0.0001)    
+        self.assertLess(abs(pr.jx()-2879.35)/2879.35,0.0001) 
+        self.assertLess(abs(pr.jy()-744.73)/744.73,0.0001) 
+        self.assertLess(abs(pr.jxy()-295)/295,0.005) 
+
+        self.assertLess(abs(pr.y1()-(0.3))/0.0001,0.0001) 
+        self.assertLess(abs(pr.y2()-(-0.3-5.23*2))/0.0001,0.005)  
+        
+        pr=profiles_sostav(pr1=pr1, alpha1=pi/2, x1=5.23-2.28+.3, y1=-5.23+2.28, mir1=0, pr2=pr1, alpha2=-pi/2, x2=-5.23+2.28-.3,y2=-5.23+2.28,mir2=1)
+        pr.solve()
+#        print pr.jy()
+
+        self.assertLess(abs(pr.xi()-0)/0.001,0.0001)
+        self.assertLess(abs(pr.yi()-2.2827)/2.2827,0.0001)
+        
+        self.assertLess(abs(pr.alpha1()-pi/2)/0.0001,0.0001)    
+        self.assertLess(abs(pr.alpha2()+pi/2)/0.0001,0.0001) 
+        self.assertLess(abs(pr.x1()-(5.23-2.28+.3))/0.0001,0.0001) 
+        self.assertLess(abs(pr.x2()-(-5.23+2.28-.3))/0.0001,0.005)
+        self.assertLess(abs(pr.y1()-(-5.23+2.28))/0.0001,0.0001) 
+        self.assertLess(abs(pr.y2()-(-5.23+2.28))/0.0001,0.005)        
+        self.assertLess(abs(pr.mir1()-0)/0.00001,0.0001) 
+        self.assertLess(abs(pr.mir2()-1)/0.0001,0.005)  
+
+        self.assertLess(abs(pr.a()-25.283*2)/25.283/2,0.0001)          
+        self.assertLess(abs(pr.jx()-204.09*2)/204.09/2,0.0001) 
+        self.assertLess(abs(pr.jy()-2879.35)/2879.35,0.001) 
+        self.assertLess(abs(pr.jxy()-0)/0.0001,0.001) 
+        
+        pr=sost_ugol_tavr_st_up(h=80, b=50, s=5, t=5, r1=8, r2=2.7, dx=10)
+        
+
+        print pr.a()
+        self.assertLess(abs(pr.a()-1274.34)/1274.34,0.0001)        
+        self.assertLess(abs(pr.jx()-0)/841074.988855,0.0001) 
+        self.assertLess(abs(pr.jy()-6157.265)/6157.265,0.0001) 
+        self.assertLess(abs(pr.xi()-0)/0.001,0.0001)
+        self.assertLess(abs(pr.yi()-2.2827)/2.2827,0.0001)                
         
 if __name__ == "__main__":
     unittest.main()
