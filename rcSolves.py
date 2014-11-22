9058ba38dadf9bbeacaf7c08aeacc7d635587b64
Mike <pma88@list.ru>
2014-11-23 00:50:47 +0300

    bugfix

--- rcSolves.py
+++ rcSolves.py
@@ -19,7 +19,8 @@ class Solves(object):
         lst=[['Rectangle',[[-1,-1],[1,1]],[100,100],1,1,[0,0,0]],
                  ['Circle',[1,0,-1],[1,1],1,2,[0,0,0]],
                  ['Circle',[1,0,1],[1,1],1,2,[0,0,0]]]
-        Создает self.formLst - список созданных материалов!'''
+        Создает self.formLst - список созданных материалов!
+        Проверено, тесты не сделаны'''
                 
         formLst=[]
         for i in lst:
@@ -36,7 +37,7 @@ class Solves(object):
         self.formGen()
 
     def centerMass(self):
-        '''возвращает координаты центра массы'''
+        '''возвращает координаты центра массы - проверено, тесты не сделаны'''
         a=0
         sx=0
         sy=0
@@ -50,12 +51,16 @@ class Solves(object):
         return x,y
         
     def formGen(self):
-        '''Создает матрицу элементов'''
+        '''Создает матрицу элементов
+        type0 - флаг если начальные искривления
+        Проверено, тесты не сделаны'''
         lst=self.formLst
         matr=False
+        first=False
         for i in lst:
-            if matr==False:
+            if first==False:
                 matr=np.array(i.mesh())
+                first=True
             else:
                 matr=np.concatenate((matr,i.mesh()) ,axis=1)
             
@@ -64,8 +69,23 @@ class Solves(object):
         self.elemMatr[0]-=x
         self.elemMatr[1]-=y
 #        print 'elemMatr', self.elemMatr
+
+        e0=(matr[-3]!=0).astype(float)
+        rx=(matr[-2]!=0).astype(float)
+        ry=(matr[-1]!=0).astype(float)
+        
+        e0sum=e0.sum()
+        rxsum=rx.sum()
+        rysum=ry.sum()
+        
+        if e0sum<10**(-10) and rxsum<10**(-10) and rysum<10**(-10):
+            self.type0=False
+        else:
+            self.type0=True
+        
     def e0rxry2e(self,e0=0,rx=0,ry=0):
-        '''создаем матрицу e'''
+        '''создаем матрицу e
+        проверено - тестов нет'''
         elemMatr=self.elemMatr #копируем матрицу, чтобы не испортить старую
         one=np.ones(elemMatr.shape[1]) #создаем матрицу 1 длиной с количества точек e
         
@@ -89,7 +109,7 @@ class Solves(object):
         d22Sum=d22.sum()
         d12Sum=d12.sum()
         
-        return [d11Sum,d12Sum,d13Sum,d22Sum,d23Sum,d33Sum]
+        return [d11Sum,d12Sum,d13Sum,d22Sum,d23Sum,d33Sum],[d11,d12,d13,d22,d23,d33]
 
 #    def e2sigma(self, e):
 #        '''возвращает матрицу sigma от e'''     
@@ -127,7 +147,8 @@ class Solves(object):
 #
     '''Версия 4 интерполяции - через функцию здесь'''
     def e2sigma4(self, e):
-        '''e - матрица e'''
+        '''e - матрица e
+        проверено - тестов нет'''
         xmatr=self.xmatr
         ymatr=self.ymatr
         kymatr=self.kymatr
@@ -135,13 +156,13 @@ class Solves(object):
 #        print xmatr.shape
 #        print self.elemMatr
         
-        boolmatr=(e<xmatr)
+        boolmatr=(e>xmatr)
 #        print boolmatr
 #        print e
 #        print xmatr
         one=np.zeros(boolmatr.shape[1])
         
-        boolmatrInvert=np.vstack((one,boolmatr[:-1]))
+        boolmatrInvert=np.vstack((boolmatr[1:],one))
         boolmatrInvert=(boolmatrInvert==False)
         boolmatr=(boolmatr==boolmatrInvert)
         
@@ -154,7 +175,9 @@ class Solves(object):
         kymatr*=boolmatr[:-1]
         kymatr=np.sum(kymatr, axis=0)
         
-        sigma=kymatr*e+ymatr
+        xmatr*=boolmatr
+        xmatr=np.sum(xmatr, axis=0)
+        sigma=kymatr*(e-xmatr)+ymatr
         
         return sigma
         
@@ -167,13 +190,13 @@ class Solves(object):
 #        print xmatr.shape
 #        print self.elemMatr
         
-        boolmatr=(e<xmatr)
+        boolmatr=(e>xmatr)
 #        print boolmatr
 #        print e
 #        print xmatr
         one=np.zeros(boolmatr.shape[1])
         
-        boolmatrInvert=np.vstack((one,boolmatr[:-1]))
+        boolmatrInvert=np.vstack((boolmatr[1:],one))
         boolmatrInvert=(boolmatrInvert==False)
         boolmatr=(boolmatr==boolmatrInvert)
         
@@ -186,67 +209,71 @@ class Solves(object):
         kymatr*=boolmatr[:-1]
         kymatr=np.sum(kymatr, axis=0)
         
-        ev=kymatr*e+ymatr
+        xmatr*=boolmatr
+        xmatr=np.sum(xmatr, axis=0)
+        
+        ev=kymatr*(e-xmatr)+ymatr
         
         return ev
         
 
 
-    '''Версия 3 интерполяции - через функцию здесь'''
-    def e2sigma3(self, e):
-        '''возвращает матрицу sigma от e'''     
-#        print self.ufunE2sigma2()(e, self.elemMatr[3])
-        return self.ufunE2sigma3()(e, self.elemMatr[3])
-
-
-    def funE2sigma3(self, x, numMat):
-        '''просто фнкция для определения e2sigma от номера материала - для перевода ее в uFun'''
-#        Вытаскиваем из lst список необходимых матриц и интеполция простым перебором
-        num=int(numMat)
-        lstx=self.lstx[num]
-        lsty=self.lsty[num]
-        lstky=self.lstky[num]
-        for i in xrange(len(lstx)-1):
-            if lstx[i]<=x<=lstx[i+1]:
-                y1=lstky[i]*x+lsty[i+1]
-                return y1
-        
-
-    def ufunE2sigma3(self):
-        '''возвращает функцию funE2ev для массивов'''
-        return np.frompyfunc(self.funE2sigma3,2,1)
-
-
-
-    '''Версия 3 интерполяции - через функцию здесь'''
-    def e2ev3(self, e):
-        '''возвращает матрицу sigma от e'''     
-#        print self.ufunE2sigma2()(e, self.elemMatr[3])
-        return self.ufunE2ev3()(e, self.elemMatr[3])
-
-
-    def funE2ev3(self, x, numMat):
-        '''просто фнкция для определения e2sigma от номера материала - для перевода ее в uFun'''
-#        Вытаскиваем из lst список необходимых матриц и интеполция простым перебором
-        num=int(numMat)
-        lstx=self.lstx[num]
-        lsty=self.lstyEv[num]
-        lstky=self.lstkyEv[num]
-        for i in xrange(len(lstx)-1):
-            if lstx[i]<=x<=lstx[i+1]:
-                y1=lstky[i]*x+lsty[i+1]
-                return y1
-        
-
-    def ufunE2ev3(self):
-        '''возвращает функцию funE2ev для массивов'''
-        return np.frompyfunc(self.funE2ev3,2,1)
+#    '''Версия 3 интерполяции - через функцию здесь'''
+#    def e2sigma3(self, e):
+#        '''возвращает матрицу sigma от e'''     
+##        print self.ufunE2sigma2()(e, self.elemMatr[3])
+#        return self.ufunE2sigma3()(e, self.elemMatr[3])
+#
+#
+#    def funE2sigma3(self, x, numMat):
+#        '''просто фнкция для определения e2sigma от номера материала - для перевода ее в uFun'''
+##        Вытаскиваем из lst список необходимых матриц и интеполция простым перебором
+#        num=int(numMat)
+#        lstx=self.lstx[num]
+#        lsty=self.lsty[num]
+#        lstky=self.lstky[num]
+#        for i in xrange(len(lstx)-1):
+#            if lstx[i]<=x<=lstx[i+1]:
+#                y1=lstky[i]*x+lsty[i+1]
+#                return y1
+#        
+#
+#    def ufunE2sigma3(self):
+#        '''возвращает функцию funE2ev для массивов'''
+#        return np.frompyfunc(self.funE2sigma3,2,1)
+#
+#
+#
+#    '''Версия 3 интерполяции - через функцию здесь'''
+#    def e2ev3(self, e):
+#        '''возвращает матрицу sigma от e'''     
+##        print self.ufunE2sigma2()(e, self.elemMatr[3])
+#        return self.ufunE2ev3()(e, self.elemMatr[3])
+#
+#
+#    def funE2ev3(self, x, numMat):
+#        '''просто фнкция для определения e2sigma от номера материала - для перевода ее в uFun'''
+##        Вытаскиваем из lst список необходимых матриц и интеполция простым перебором
+#        num=int(numMat)
+#        lstx=self.lstx[num]
+#        lsty=self.lstyEv[num]
+#        lstky=self.lstkyEv[num]
+#        for i in xrange(len(lstx)-1):
+#            if lstx[i]<=x<=lstx[i+1]:
+#                y1=lstky[i]*x+lsty[i+1]
+#                return y1
+#        
+#
+#    def ufunE2ev3(self):
+#        '''возвращает функцию funE2ev для массивов'''
+#        return np.frompyfunc(self.funE2ev3,2,1)
 
     
 
 
     def e0rxry2nmxmy(self,e0rxry):
-        '''возвращает значение усилий по дополнительным деформациям'''
+        '''возвращает значение усилий по дополнительным деформациям
+        проверено - тестов нет'''
         e0,rx,ry=e0rxry
         e=self.e0rxry2e(e0,rx,ry) #получили e1
 #        print self.e2sigma2(e),'sigma' 
@@ -257,7 +284,7 @@ class Solves(object):
         nSum=n.sum()
         mxSum=mx.sum()
         mySum=my.sum()
-        return nSum, mxSum, mySum
+        return nSum, mxSum, mySum, sigma, e
         
         
     def matrSolve(self, nmxmy, dd):
@@ -278,19 +305,42 @@ class Solves(object):
 
     def nmxmy2e0rxry(self,nmxmy, nn, crit):
         '''определение e0rxry, nn - макс кол-во итераций, criter - критерий сходимости
+        type0 - учет начального искривления  False - нет искривлени - считается автоматом
         Возвращает список: e0f, rxf, ryf, tol
         При ошибке tol==string'''
+        elemMatr=self.elemMatr
+        
+        type0=self.type0
+                 
         e0,rx,ry=0,0,0
-#        dn, dmx, dmy=self.e0rxry2nmxmy([e0,rx,ry])
-#        nmxmy=[nmxmy[0]+dn,nmxmy[1]+dmx,nmxmy[2]+dmy]
+        if type0==True:
+            dn, dmx, dmy=self.e0rxry2nmxmy([e0,rx,ry])[0:3]
+            nmxmy=[nmxmy[0]+dn,nmxmy[1]+dmx,nmxmy[2]+dmy]
         n=0
         while True:
             ee=self.e0rxry2e(e0,rx,ry)
-            dd=self.e2d(ee)            
-            e0f,rxf,ryf,error=self.matrSolve(nmxmy,dd)
+            dd=self.e2d(ee)
+            
+            if type0==True:
+                d11,d12,d13,d22,d23,d33= dd[1]           
+                dnTemp=d33*elemMatr[-3]+d13*elemMatr[-2]+d23*elemMatr[-1]
+                dnSum=dnTemp.sum()
+                
+                
+                dmxTemp=d13*elemMatr[-3]+d11*elemMatr[-2]+d12*elemMatr[-1]
+                dmxSum=dmxTemp.sum()
+    
+    
+                dmyTemp=d23*elemMatr[-3]+d12*elemMatr[-2]+d22*elemMatr[-1]
+                dmySum=dmyTemp.sum()
+    
+                nmxmyTemp=[nmxmy[0]-dnSum,nmxmy[1]-dmxSum,nmxmy[2]-dmySum]
+            else:
+                nmxmyTemp=nmxmy
+            e0f,rxf,ryf,error=self.matrSolve(nmxmyTemp,dd[0])
             
             if error==1:
-                return [False, False, False, 'Matr']
+                return [False, False, False, 'Matr',[]]
             else:
                 if e0f==0:
                     tolE=abs(e0f-e0)
@@ -311,72 +361,48 @@ class Solves(object):
                 tol=max(tolE, tolRx, tolRy)
 
                 if tol<crit:
-                    return [e0f, rxf, ryf, tol]
+                    return [e0f, rxf, ryf, tol, dd[0]]
                 
                 if n>nn:
-                    return [False, False, False, 'Nmax']
+                    return [False, False, False, 'Nmax',[]]
                 
                 e0, rx,ry=e0f, rxf, ryf  
                 n+=1
                 
         
     
-    def loadMat(self, lstMat, typLst, lst, typDia, typPS, typTime, typR, typRT):
-        '''загружаем материалы и создаем список функций 
-        lstMat - список созднных материалов
-        typLst - если True - диаграмма создается по списку lst, 
-        lst - cписок точек для typLst=True, 
-        typDia - тип диаграммы для бетонна, 
-        typPS - тип предельного состояния, 
-        typTime - long или short для бетона, 
-        typR - для бетона, если 1 - просто режется все -, если 2 - после последнего значения - до 0, другое - продлеваем до max, 
-        typRT - для бетона, если 1 - просто режется все +, если 2 - после последнего значения - до 0, другое - продлеваем до max'''
-        self.lstMat=lstMat
-        lstFunDia=[]
-        for i in lstMat:
-            if i.title()=='Concrete':
-                if typLst==False:
-                    lstFunDia.append(i.functDia(typDia, typPS, typTime, typR, typRT))
-                else:
-                    lstFunDia.append(i.functDiaLst(lst))
-            elif i.title()=='Reinforced':
-                if typLst==False:
-                    lstFunDia.append(i.functDia(typPS))
-                else:
-                    lstFunDia.append(i.functDiaLst(lst))
-        self.lstFunE2ev=lstFunDia
+    def loadMat(self, lstMat):
+        '''загружаем материалы и создаем список функций
+        в принципе правильно - без тестов''' 
 
-        lstFunDia2=[]
-        
+        self.lstMat=lstMat
+
+        ''' записываем в lst все массивы'''  
+        maxx=0
         lstx=[]
         lsty=[]
         lstky=[]
         lstyEv=[]
         lstkyEv=[]
         for i in lstMat:
-            if i.title()=='Concrete':
-                if typLst==False:
-#                    lstFunDia2.append(i.functDia2)
-                    lstx.append(i.x)
-                    lsty.append(i.y)
-                    lstky.append(i.ky)
-                    lstyEv.append(i.yEv)
-                    lstkyEv.append(i.kyEv)
+            if len(i.x)>maxx:
+                maxx=len(i.x)
 
-                else:
-                    lstFunDia.append(i.functDiaLst(lst))
-            elif i.title()=='Reinforced':
-                if typLst==False:
-                    lstFunDia.append(i.functDia2())
-                else:
-                    lstFunDia.append(i.functDiaLst(lst))
+            lstx.append(i.x)
+            lsty.append(i.y)
+            lstky.append(i.ky)
+            lstyEv.append(i.yEv)
+            lstkyEv.append(i.kyEv)
                     
-        self.lstFunE2ev2=lstFunDia2
-        self.lstx=lstx
-        self.lsty=lsty
-        self.lstky=lstky
-        self.lstyEv=lstyEv
-        self.lstkyEv=lstkyEv
+        '''приводим все к одному значению'''
+        for j in range(len(lstx)):
+            if len(lstx[j])<maxx:
+                for i in range(maxx-len(lstx[j])):
+                    lstx[j]=np.append(lstx[j], lstx[j][-1])
+                    lsty[j]=np.append(lsty[j], lsty[j][-1])
+                    lstky[j]=np.append(lstky[j], lstky[j][-1])
+                    lstyEv[j]=np.append(lstyEv[j], lstyEv[j][-1])
+                    lstkyEv[j]=np.append(lstkyEv[j], lstkyEv[j][-1])
 
         '''теперь создаем матрицу соответствующую № массива'''
         
@@ -429,6 +455,7 @@ class Solves(object):
                 kymatr=np.hstack((kymatr,kymatrTemp))
                 yEvmatr=np.hstack((yEvmatr,yEvmatrTemp))
                 kyEvmatr=np.hstack((kyEvmatr,kyEvmatrTemp))
+                
         self.xmatr=xmatr
         self.ymatr=ymatr
         self.kymatr=kymatr
@@ -446,49 +473,60 @@ class Solves(object):
 
     
 if __name__ == "__main__":
-    lstForm=[['Rectangle',[[1.,1.],[3.,3.]],[50.,50.],0,1,[0,0,0]]]
+    lstForm=[
+    ['Rectangle',[[0.,0.],[50.,50.]],[100.,100.],0,1,[0,0,0]],
+    ['Circle',[5,5,2],[],1,1,[0,0,0]],
+    ['Circle',[45,45,2],[],1,1,[0,0,0]],
+    ['Circle',[45,5,2],[],1,1,[0,0,0]],
+    ['Circle',[5,45,2],[],1,1,[0,0,0]]
+    ]    
     
     sol=Solves()
     sol.loadForm(lstForm)
-#    print sol.formLst[0].a()  
-#    print sol.elemMatr
-#    print sol.elemMatr.shape[1]
-#    print sol.centerMass()
+    lst=sol.elemMatr
+    a=lst[2].sum()
+#    print a
+
     conc=rcMaterial.Concrete()
     conc.norme=52
     conc.b=25
     conc.initProperties()
-#    
-#    reinf=rcMaterial.Reinforced()
-#    reinf.rn=400
-#    reinf.typ='A'
-#    reinf.initProperties()
-    lstMat=[conc]
+    conc.functDia(typDia=2, typPS=1,typTime='short', typR=0, typRT=2)
+    
+    rein=rcMaterial.Reinforced()
+    rein.norme=52
+    rein.typ='A'
+    rein.a=400
+    rein.initProperties()
+    rein.functDia(typPS=1)
+    
+    lstMat=[conc, rein]
 
-    sol.loadMat(lstMat, typLst=False, lst=None, typDia=2, typPS=1, typTime='short', typR=3, typRT=3)
+    sol.loadMat(lstMat)
+    xmatr=sol.xmatr
+#    print xmatr
+
+    ymatr=sol.ymatr
+#    print ymatr
+
     
-    for i in range(10000):
+    e0rxry=[0.000501077552827452,0.0000272026093313562,0.0000272026093313562]
+    out=sol.e0rxry2nmxmy(e0rxry)
+    e=np.reshape(out[4][0:100], (10, 10))
+    sigma=np.reshape(out[3][0:100], (10, 10))
+    print out[0:3]
     
-#        e0rxry=[-0.0025,0,0]
-#    #    
-#        sol.e0rxry2nmxmy(e0rxry)
-        
-        nmxmy=[-1,0,0]
-        
-        sol.nmxmy2e0rxry(nmxmy,100,0.001)
-#        
-    nmxmy=[-2,0,0]
-    
-    print sol.nmxmy2e0rxry(nmxmy,100,0.001)
-#        
+    nmxmy=[0,5,5]
+    out=sol.nmxmy2e0rxry(nmxmy, 1000, 0.0001)
+    print out
+#    for i in range(10000):
 #    
-#def interpol(x):
-#    lstx=[1,2,3,4,5,6]
-#    lsty=[1,2,3,4,5,6]
-#    for i in range(len(lstx)-1):
-#        if lstx[i]>=x and lstx[i+1]<=x:
-#            y=(lsty[i]-lsty[i+1])/(lstx[i]-lstx[i+1])+lsty[i+1]
-#            return y
-#for i in range(100*100*100):
-#    interpol(3.5)
+#        
+#        nmxmy=[-1,0,0]
+#        
+#        sol.nmxmy2e0rxry(nmxmy,100,0.001)
+#       
+#    nmxmy=[-2,0,0]
+#    
+#    print sol.nmxmy2e0rxry(nmxmy,100,0.001)
 print 'ok'
